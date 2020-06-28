1a1d4a1f9998abf8152cbff7f409e3ae16c56138
marco <matoro546@gmail.com>
2020-06-28 11:31:30 +0200

    completamento carrello (forse)

--- gusci_php/gusci_host/tameplate_banner.php
+++ gusci_php/gusci_host/tameplate_banner.php
@@ -70,34 +70,60 @@
                 </div>   
             </div>
 
-            <div style="height: fit-content;position: absolute;top: 40%;right: 7%;">
-                <h1 style="position: relative;display: flex;margin-bottom: 10px;justify-content: center;">Prezzo: <?php echo $articolo['prezzo'] ?> &#x20ac</h1>
-                <hr>
-                <?php echo '<form action="tameplate_banner.php?articolo='.$_GET['articolo'].'" method="POST">'?>
-                <input type="submit" class="bottoni" name="submit_add_cart" style="margin-top: 15px;" value="aggiungi al carrelo"> 
-                </form>
+            <div style="height: fit-content;position: absolute;top: 30%;right: 7%;">
+                <h1 style="position: relative;display: flex;margin-bottom: 30px;justify-content: center;">Prezzo: <?php echo $articolo['prezzo'] ?> &#x20ac</h1>
+<?php 
+    $conn=mysqli_connect("localhost"," gusci","","my_gusci");
+    $quantita=mysqli_query($conn,"select quantita from carrello where idart=".addslashes($articolo['idart'])." and iduser=".addslashes($_SESSION['iduser']).";");
+    $quantita=mysqli_fetch_array($quantita);
+    if($quantita[0]<10)
+    {
+        echo '<form action="tameplate_banner.php?articolo='.$_GET['articolo'].'" method="POST">';
+        echo '<div style="display: flex;justify-content: center;margin-bottom: 10px;">';
+        echo '<h3 style="height: fit-content; top: 8px; margin-right: 10px; position: relative;">Quantità:</h3>';
+        echo '<input type="number" class="dati_prodotto" name="quantita" style="font-size: 20px;width: 40px;margin-bottom: 15px;" value="1" min="1" max="10"> ';
+        echo '</div>';
+        echo '<h3 style="height: fit-content; top: 8px;margin-bottom: 20px; position: relative;">'.$quantita[0].' già nel tuo carrello </h3>';
+        echo '<hr>';
+        echo '<input type="submit" class="bottoni" name="submit_add_cart" style="margin-top: 15px;" value="aggiungi al carrelo">';
+        echo '</form>';
+
+    }
+    else
+    {
+        echo '<h3 style="height: fit-content; top: 8px;margin-bottom: 20px; position: relative; width:300px "> non puoi più aggiungere questo articolo hai raggiunto la quantita massima per singolo articolo di 10</h3>';
+        echo '<hr>';
+        echo '<button class="bottoni_fake">aggiungi al carrelo</button> ';
+
+    }
+                
+?>
             </div>
         </div>
 <?php
     if(isset($_POST['submit_add_cart']))
     {
-        $conn=mysqli_connect("localhost"," gusci","","my_gusci");
+        
         $carrello_art=mysqli_query($conn,"select idart from carrello where idart=".addslashes($articolo['idart'])." and iduser=".addslashes($_SESSION['iduser']).";");
         $carrello_art=mysqli_fetch_assoc($carrello_art);
         if(strlen($carrello_art['idart'])<=0)
         {
-            mysqli_query($conn,"insert into carrello values(".$articolo['idart'].",".addslashes($_SESSION['iduser']).",1);");
+            mysqli_query($conn,"insert into carrello values(".addslashes($articolo['idart']).",".addslashes($_SESSION['iduser']).",".addslashes($_POST['quantita']).");");
         }
         else
         {
             $quantita=mysqli_query($conn,"select quantita from carrello where idart=".addslashes($articolo['idart'])." and iduser=".addslashes($_SESSION['iduser']).";");
             $quantita=mysqli_fetch_array($quantita);
-            $i=$quantita[0]+1;
+            $i=$quantita[0]+$_POST['quantita'];
+            if($i>10)
+                $i=10;
+
             mysqli_query($conn,"update carrello set quantita=$i;");
 
 
         }
 
+        mysqli_close($conn);
     }
 ?>
 </body>
