900c33b9ab8e95787eafc4c329e36682aa79f5d7
marco <matoro546@gmail.com>
2020-11-05 15:36:48 +0100

    ore di info

---
+++ compiti info/num-let/num-let.cpp
@@ -0,0 +1,207 @@
+//marco schiavello
+//dato un numero scriverlo in forma letterale
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+int main()
+{
+	int n,u,d,c,m;
+	
+	do
+	{
+		printf("inserire un numero (tra 1 e 9999 ) : ");
+		scanf("%d",&n);		
+	}
+	while(n<0||n>9999);
+	
+	m=n/1000;
+	c=(n-(m*1000))/100;
+	d=(n-((c*100)+(m*1000)))/10;
+	u=(n-((c*100)+(d*10)+(m*1000)))/1;
+	printf("%d,%d,%d,%d \n",m,c,d,u);
+	switch (m)
+	{
+		case 2:
+			printf("due ");
+		break;
+		case 3:
+			printf("tre ");
+		break;
+		case 4:
+			printf("quatro ");
+		break;
+		case 5:
+			printf("cinque ");
+		break;
+		case 6:
+			printf("sei ");
+		break;
+		case 7:
+			printf("sette ");
+		break;
+		case 8:
+			printf("otto ");
+		break;
+		case 9:
+			printf("nove ");
+		break;
+	}
+	if(m==1)
+		printf("mille ");
+	else if(m!=0)
+		printf("mila ");
+	switch (c)
+	{
+		case 2:
+			printf("due ");
+		break;
+		case 3:
+			printf("tre ");
+		break;
+		case 4:
+			printf("quatro ");
+		break;
+		case 5:
+			printf("cinque ");
+		break;
+		case 6:
+			printf("sei ");
+		break;
+		case 7:
+			printf("sette ");
+		break;
+		case 8:
+			printf("otto ");
+		break;
+		case 9:
+			printf("nove ");
+		break;
+	}
+	if(c!=0)
+		printf("cento ");
+		
+	switch (d)
+	{
+		case 1:
+			switch (u)
+			{
+				case 0:
+					printf("dieci ");
+				break;
+				case 1:
+					printf("undici ");
+				break;
+				case 2:
+					printf("dodici ");
+				break;
+				case 3:
+					printf("tredici ");
+				break;
+				case 4:
+					printf("quattordici ");
+				break;
+				case 5:
+					printf("quindici  ");
+				break;
+				case 6:
+					printf("sedici ");
+				break;
+				case 7:
+					printf("diciassette ");
+				break;
+				case 8:
+					printf("diciotto ");
+				break;
+				case 9:
+					printf("diciannove ");
+				break;
+			}
+		break;
+		case 2:
+			if(u==1||u==8)
+				printf("vent ");
+			else
+				printf("venti ");	
+		break;
+		case 3:
+			if(u==1||u==8)
+				printf("trent ");
+			else
+				printf("trenta ");	
+		break;
+		case 4:
+			if(u==1||u==8)
+				printf("quarant ");
+			else
+				printf("quaranta ");	
+		break;
+		case 5:
+			if(u==1||u==8)
+				printf("cinquant ");
+			else
+				printf("cinquanta ");	
+		break;
+		case 6:
+			if(u==1||u==8)
+				printf("sessant ");
+			else
+				printf("sessanta ");	
+		break;
+		case 7:
+			if(u==1||u==8)
+				printf("settant ");
+			else
+				printf("settanta ");	
+		break;
+		case 8:
+			if(u==1||u==8)
+				printf("ottant ");
+			else
+				printf("ottanta ");	
+		break;
+		case 9:
+			if(u==1||u==8)
+				printf("novant ");
+			else
+				printf("novanta ");	
+		break;
+	}
+	if(d!=1)
+		switch (u)
+		{
+			case 0:
+				printf("zer0 ");
+			break;
+			case 1:
+				printf("uno ");
+			break;
+			case 2:
+				printf("due ");
+			break;
+			case 3:
+				printf("tre ");
+			break;
+			case 4:
+				printf("quatro ");
+			break;
+			case 5:
+				printf("cinque  ");
+			break;
+			case 6:
+				printf("sei ");
+			break;
+			case 7:
+				printf("sette ");
+			break;
+			case 8:
+				printf("otto ");
+			break;
+			case 9:
+				printf("nove ");
+			break;
+		}
+
+	printf("\n");
+	getch();
+}
