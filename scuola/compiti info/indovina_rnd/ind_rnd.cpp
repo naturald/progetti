8590b3ec0925a22c9b31fbd555f2561d61f48e68
naturald <marschi.gamer@gmail.com>
2020-11-13 10:12:33 +0100

    es info

--- scuola/compiti info/indovina_rnd/ind_rnd.cpp
+++ scuola/compiti info/indovina_rnd/ind_rnd.cpp
@@ -8,14 +8,13 @@
 int main()
 {
 	srand(time(0));
-	int n=rand()%10000+1,ind,i=0;
+	int n=rand()%10000+1,ind;
 	//printf("%d\n",n);
-	while(i<14)
+	for(int i=0;i<14;i++)
 	{
-		i++;
 		do
 		{
-			printf("%d )prova a indovinare: ",i); 
+			printf("%d )prova a indovinare: ",i+1); 
 			scanf("%d",&ind);
 		}
 		while(ind<0||ind>10000);
@@ -23,9 +22,9 @@ int main()
 			printf("hai indovinato HAI VINTO\n");
 		else if(ind<n)
 			printf("troppo basso\n");
-		else if(ind>n)
+		else 
 			printf("troppo alto\n");
 	}
-	printf("HAI PERSO")
+	printf("HAI PERSO");
 	getch();
 }
