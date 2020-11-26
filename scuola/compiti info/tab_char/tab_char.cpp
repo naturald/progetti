af3502b0db27c34fd84f306bf6924737f5721d4c
naturald <marschi.gamer@gmail.com>
2020-11-26 15:15:45 +0100

    programma aggiornato date

--- scuola/compiti info/tab_char/tab_char.cpp
+++ scuola/compiti info/tab_char/tab_char.cpp
@@ -3,15 +3,15 @@
 #include <stdio.h>
 #include <conio.h>//TEORICAMENTE SERVE IL 2 PER TEXTCOLOR(COLOR_NAME)
 #include <windows.h>//system ("cls")
-#define N_RIGHE 14
+#define N_COL 19
 
 void linea(int volte,bool invio=false,bool trat=false)
 {
 	for(int i=0;i<volte;i++)
 	{
-		printf("o-----");		
+		printf("+-----");		
 	}
-	printf("o");
+	printf("+");
 	if(invio==true)
 		printf("\n");
 	if(trat==true)
@@ -21,20 +21,23 @@ void linea(int volte,bool invio=false,bool trat=false)
 
 int main()
 {
-	linea(N_RIGHE,true,true);
+	int col=0;
+	linea(N_COL,true,true);
 	for(int i=0;i<256;i++)
 	{
-		if(i%N_RIGHE==0&&i!=0)
+		if(i%N_COL==0&&i!=0)
 		{
 			printf("\n");
-			linea(N_RIGHE,true,true);	
+			linea(N_COL,true,true);	
+			col=0;
 		}
 
 		if(i<=13)
 			printf("%3d %c|",i,14);
 		else
 			printf("%3d %c|",i,i);
+		col++;
 	}
 	printf("\n");
-	linea(N_RIGHE,true,false);;
+	linea(col,true,false);
 }
