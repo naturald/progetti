4cce10f5aeada2f47a5a49fbffd31a37a7f77348
naturald <marschi.gamer@gmail.com>
2020-12-04 16:17:47 +0100

    compiti info

--- scuola/compiti info/marcoLib.h
+++ scuola/compiti info/marcoLib.h
@@ -13,10 +13,11 @@ void tabella(int volte,bool invio=false,bool trat=false)
 		printf("|");
 		
 }
-void leggi_int(char messagio[],int da_leg) 
+int leggi_int(char messagio[],int da_leg) 
 {
 	printf("%s",messagio);
 	scanf("%d",&da_leg);
+	return da_leg;
 }
 float pow(int base,int esp,int volte=0,int ris=1,bool neg=false)
 {
@@ -39,3 +40,37 @@ float pow(int base,int esp,int volte=0,int ris=1,bool neg=false)
 	return pow(base,esp,volte,ris);
 	
 }
+void goToXY(int x,int y)
+{
+  system("cls");
+  for(int i=0;i<y;i++)
+    printf("\n");
+  for(int j=0;j<x;j++)
+    printf(" ");
+}
+void tab_ascii(int N_COL)
+{
+	int col=0;
+	tabella(N_COL,true,true);
+	for(int i=0;i<256;i++)
+	{
+		if(i%N_COL==0&&i!=0)
+		{
+			printf("\n");
+			tabella(N_COL,true,true);	
+			col=0;
+		}
+
+		if(i<=13||i==27)
+			printf("%3d %c|",i,14);
+		else
+			printf("%3d %c|",i,i);
+		col++;
+	}
+	printf("\n");
+	tabella(col,true);
+
+	getch();
+}
+
+
