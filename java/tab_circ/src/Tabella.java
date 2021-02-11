451e8a37541bb9546f95f102d857ac9f75ce274c
naturald <marschi.gamer@gmail.com>
2021-02-11 10:33:23 +0100

    update

--- java/tab_circ/src/Tabella.java
+++ java/tab_circ/src/Tabella.java
@@ -16,29 +16,29 @@ public class Tabella
     }
     public void stamTab()
     {
-        System.out.println("--------------------------------------------------------------------------------");
-        System.out.println("| "+strLen("Circ",4)+" | "+strLen("relazione",10)+" | "+strLen("tra quali R",10)+" | "+strLen("comp I",15)+" | "+strLen("comp V",15)+" | "+strLen("Rxy",6)+" |");
-        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.println("| "+strLen("Circ",4)+" | "+strLen("relazione",10)+" | "+strLen("tra quali R",13)+" | "+strLen("comp I",17)+" | "+strLen("comp V",17)+" | "+strLen("Rxy",6)+" |");
+        System.out.println("--------------------------------------------------------------------------------------");
         for(Voci i : contenuto)
         {
             if(i != null)
             {
-                System.out.print("| " + strLen(i.nCirc+"",4) + " | " + strLen(i.tipoCon,10) + " | " + strLen(i.tra_r[0] + "," + i.tra_r[1],11)+ " | " + strLen(i.compV ,15)+ " | " + strLen(i.compI,15) + " | " + strLen((Math.floor(i.Rxy * 100) / 100)+"",6) + " | \n");
-                System.out.println("--------------------------------------------------------------------------------");
+                System.out.print("| " + strLen(i.nCirc+"",4) + " | " + strLen(i.tipoCon,10) + " | " + strLen(i.tra_r[0] + "," + i.tra_r[1],13)+ " | " + strLen(i.compV ,17)+ " | " + strLen(i.compI,17) + " | " + strLen((Math.floor(i.Rxy * 100) / 100)+"",6) + " | \n");
+                System.out.println("--------------------------------------------------------------------------------------");
             }
         }
     }
     public void stamRes(Resistenza[] res)
     {
 
-        System.out.println("o------o------o");
-        System.out.println("| "+strLen("Nome",4)+" | "+strLen("Valo",4)+" |");
-        System.out.println("o------o------o");
+        System.out.println("o------------o------o");
+        System.out.println("| "+strLen("Nome",10)+" | "+strLen("Valo",4)+" |");
+        System.out.println("o------------o------o");
         for(Resistenza i : res)
             if(i != null)
             {
-                System.out.println("| " + strLen(i.getNome(), 4) + " | " + strLen(i.getValore() + "", 4) + " |");
-                System.out.println("o------o------o");
+                System.out.println("| " + strLen(i.getNome(), 10) + " | " + strLen(i.getValore() + "", 4) + " |");
+                System.out.println("o------------o------o");
             }
     }
     public static String strLen(String string, int length) { return String.format("%1$"+length+"s", string); }
